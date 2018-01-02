34466eab918d74e1bef48422039e9f0d681f2e33
ramazanov.iteam <ramazanov.iteam@gmail.com>
2018-01-02 23:43:30 +0600

    Первый коммит

---
+++ app/src/main/java/com/iteam_ramazanov/publicchatroomclientapp/PublicChatRoomClientActivity.java
@@ -0,0 +1,13 @@
+package com.iteam_ramazanov.publicchatroomclientapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PublicChatRoomClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_chat_room_client);
+    }
+}
