672f8e713c78a9067cdefa988ef7b4ba462b1c7e
ramazanov.iteam <ramazanov.iteam@gmail.com>
2018-01-03 00:26:41 +0600

    Реализовал отправку сообщении

--- app/src/main/java/com/iteam_ramazanov/publicchatroomclientapp/PublicChatRoomClientActivity.java
+++ app/src/main/java/com/iteam_ramazanov/publicchatroomclientapp/PublicChatRoomClientActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -18,10 +19,15 @@ public class PublicChatRoomClientActivity extends AppCompatActivity implements T
     public void btnSendOnClick(View v) {
         EditText edtHost = (EditText) findViewById(R.id.edtHost);
         EditText edtPort = (EditText) findViewById(R.id.edtPort);
+        EditText edtNickName = (EditText) findViewById(R.id.edtNickName);
+        EditText edtMessage = (EditText) findViewById(R.id.edtMessage) ;
         String ip_address = edtHost.getText().toString();
         int port = Integer.parseInt(edtPort.getText().toString());
+        String nick_name = edtNickName.getText().toString();
+        String message = edtMessage.getText().toString();
         try {
             TCPConnection connection = new TCPConnection(this, ip_address, port);
+            connection.sendString(nick_name + ": " + message);
         } catch (IOException e) {
             printMsg("Connection exception: " + e);
         }
@@ -48,7 +54,7 @@ public class PublicChatRoomClientActivity extends AppCompatActivity implements T
     }
 
     private synchronized void printMsg(String message) {
-        EditText edtNickName = (EditText) findViewById(R.id.edtNickName);
-        edtNickName.append(message);
+        TextView tvLogs = (TextView) findViewById(R.id.tvLogs);
+        tvLogs.append(message);
     }
 }
